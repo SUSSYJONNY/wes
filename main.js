f5bc3243cc248cdd70809db122da6a1531b4dc66
SUSSYJONNY <128854842+SUSSYJONNY@users.noreply.github.com>
2024-04-27 14:38:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -54,52 +54,63 @@ function gotPoses() {
 
     rightWristY = results[0].pose.rightWrist.y;
     rightWristX = results[0].pose.rightWrist.y;
-    ScoreRightWrist = results[0].pose.keypoints[10].score;
+    scoreRightWrist = results[0].pose.keypoints[10].score;
     console.log(scoreRightWrist);
   }
 }
 
+function startGame() {
+  gameStatus = "start";
+  document.getElementById("status").innerHTML = "Jogo foi carregado"
+}
 
 function draw() {
+  if (gameStatus == "start") {
+    background(0);
 
-  background(0);
+    fill("black");
+    stroke("black");
+    rect(680, 0, 20, 700);
 
-  fill("black");
-  stroke("black");
-  rect(680, 0, 20, 700);
+    if (scoreRightWrist > 0.2) {
+      fill("red");
+      stroke("black");
+      circle(rightWristX, rightWristY, 30);
+    }
+    fill("black");
+    stroke("black");
+    rect(0, 0, 20, 700);
 
-  fill("black");
-  stroke("black");
-  rect(0, 0, 20, 700);
+    //Chamar a função paddleInCanvas() 
+    paddleInCanvas();
 
-  //Chamar a função paddleInCanvas() 
-  paddleInCanvas();
-
-  //Raquete do jogador
-  fill(250, 0, 0);
-  stroke(0, 0, 250);
-  strokeWeight(0.5);
-  paddle1Y = rightWristY;
-  rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
+    //Raquete do jogador
+    fill(250, 0, 0);
+    stroke(0, 0, 250);
+    strokeWeight(0.5);
+    paddle1Y = rightWristY;
+    rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
 
 
-  //Raquete do computador
-  fill("#FFA500");
-  stroke("#FFA500");
-  paddle2y = ball.y - paddle2Height / 2;
-  rect(paddle2Y, paddle2y, paddle2, paddle2Height, 100);
+    //Raquete do computador
+    fill("#FFA500");
+    stroke("#FFA500");
+    paddle2y = ball.y - paddle2Height / 2;
+    rect(paddle2Y, paddle2y, paddle2, paddle2Height, 100);
 
-  //Chamar a função midline()
-  midline();
+    //Chamar a função midline()
+    midline();
 
-  //Chamar a função drawScore() 
-  drawScore();
+    //Chamar a função drawScore() 
+    drawScore();
 
-  //Chamar a função models()  
-  models();
+    //Chamar a função models()  
+    models();
 
-  //Chamar a função move() (muito importante para o jogo)
-  move();
+    //Chamar a função move() (muito importante para o jogo)
+    move();
+
+  }
 }
 
 
